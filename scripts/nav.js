806087591fb67a34517c21fd37e3706ba5447423
Dawid Lechowski <david.lechowski@gmail.com>
2021-12-27 19:16:47 +0100

    next

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,7 +1,7 @@
 const navBcg = document.querySelector(".nav_background");
 const nav = document.querySelector("nav");
 const navInside = document.querySelector(".nav_inside");
-const buttons = document.querySelector(".buttons");
+const buttons = document.querySelector(".buttons__inner");
 const logo = document.querySelector(".logo img")
 
 
@@ -10,14 +10,12 @@ window.addEventListener("scroll", function () {
     let scrollPosition = window.scrollY
     if (scrollPosition > 0) {
         navBcg.classList.add("on");
-        nav.classList.add("fixed");
         navInside.classList.add("centered")
         buttons.classList.add("moved")
         logo.classList.add("smaller")
     }
     else {
         navBcg.classList.remove("on");
-        nav.classList.remove("fixed");
         navInside.classList.remove("centered");
         buttons.classList.remove("moved");
         logo.classList.remove("smaller")
